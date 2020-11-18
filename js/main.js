27079f30b2269cfafaefdbeee4f0b806851ea404
Matteo Ponchietti <matteoponchietti@gmail.com>
2020-11-18 18:36:47 +0000

    FINITO (forse)

--- js/main.js
+++ js/main.js
@@ -6,10 +6,26 @@ const appToDo = new Vue({
   },
   methods: {
     addItemList: function(){
-      this.toDoList.push(this.userInput);
+      let newToDoItem = {
+        textToDo: this.userInput,
+        isChecked: false
+      }
+      this.toDoList.push(newToDoItem);
       this.userInput = '';
     },
-    removeItemList: function(removeIndex){
-      this.toDoList.splice(removeIndex, 1);
+    removeItemList: function(j){
+      this.toDoList.splice(j, 1)
+    },
+    toggleCheck: function(i){
+      if(this.toDoList[i].isChecked == false){
+        this.toDoList[i].isChecked = true
+        console.log(this.toDoList[i].isChecked)
+
+      } else{
+        this.toDoList[i].isChecked = false
+        console.log(this.toDoList[i].isChecked)
+      }
     }
+
+  }
 });
